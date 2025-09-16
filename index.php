3533f54668570788b09fb491e08c00fea9ed94e1
Prof. Tushar Gohil <53966413+gtushar2910@users.noreply.github.com>
2025-09-16 08:17:12 +0000

    push

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Bref\Event\Http\FpmHandler;
+
+// Point to your actual application entry file:
+$handler = new FpmHandler('src/index1.php');
+$handler->run();
+?>
